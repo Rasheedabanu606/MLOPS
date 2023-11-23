a9f587f24e9e40132371ef6fd2318c0bf05868d4
Rasheeda Banu Y <148965699+Rasheedabanu606@users.noreply.github.com>
2023-11-23 12:40:16 +0530

    Update and rename demo_lin_reg_fit.py to demo_lin_reg_webapp.py

---
+++ demo_lin_reg_webapp.py
@@ -0,0 +1,16 @@
+model = pickle.load(file)
+
+# Create the Streamlit web app
+st.header("Streamlit demo")
+
+st.sidebar.header("This is a web app")
+
+X_test = st.sidebar.slider("Select X to get yhat", 0, 10, 5)
+
+st.write("X test is:", X_test)
+
+yhat_test = model.predict([[X_test]])
+
+st.write("b0 is", round(model.intercept_, 3))
+st.write("b1 is", round(model.coef_[0], 3))
+st.write("yhat test is", yhat_test)
